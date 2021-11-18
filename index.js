b400bd0c2901b76da74d6383fc99eb1d3287115c
Monica Björk <m.bjork95@gmail.com>
2021-11-18 22:40:47 +0100

    new commands

--- index.js
+++ index.js
@@ -17,57 +17,23 @@ for (const file of commandFiles) {
 }
 
 // When the client is ready, run this code (only once)
-//client.once('ready', () => {
-//    console.log('Ready!');
-//});
-
-client.on('debug', console.log);
-
-
-client.on('ready', () => {
-    console.log('Application thingy is ready');
-    client.api.applications(client.user.id).guilds('898847765814186044').commands.post({
-        data: {
-            name: "hello",
-            description: "hello world command"
-            // possible options here e.g. options: [{...}]
-        }
-    });
-
-
-    client.ws.on('INTERACTION_CREATE', async interaction => {
-        const command = interaction.data.name.toLowerCase();
-        const args = interaction.data.options;
-
-        if (command === 'hello'){ 
-            console.log('Command: hello');
-            // here you could do anything. in this sample
-            // i reply with an api interaction
-            client.api.interactions(interaction.id, interaction.token).callback.post({
-                data: {
-                    type: 4,
-                    data: {
-                        content: "hello world!"
-                    }
-                }
-            })
-        }
-    });
+client.once('ready', () => {
+    console.log('Ready!');
 });
 
+//client.on('debug', console.log);
 
 
-// Commands
 client.on('interactionCreate', async interaction => {
-    console.log('is this ping?');
-	if (!interaction.isCommand()) return;
-
+    if (!interaction.isCommand()) return;
+    
 	const command = client.commands.get(interaction.commandName);
-
+    
 	if (!command) return;
-
+    
 	try {
-		await command.execute(interaction);
+        await command.execute(interaction);
+        console.log('Command executed: ', interaction.commandName );
 	} catch (error) {
 		console.error(error);
 		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
