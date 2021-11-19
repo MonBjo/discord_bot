dd0ad2efa71a2d6c05c5e8577c582871731c34a9
Monica Björk <m.bjork95@gmail.com>
2021-11-19 21:11:47 +0100

    I need to find intents

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const auditlogChannel = client.channels.cache.get('911274099286876220');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 
-// Logs in to Discord
+const auditlogChannel = { key:null };
+
 client.login(token);
 
 client.once('ready', () => {
 	console.log('Ready!');
-	console.log(auditlogChannel);
-	//auditlogChannel.send('Hi!');
-	client.user.setActivity('villagers in Ironwatch', { type: 'WATCHING' });
+	client.user.setActivity('villagers', { type: 'WATCHING' });
+
+	auditlogChannel.key = client.channels.cache.get('911274099286876220');
+	// console.log(auditlogChannel.key);
+	auditlogChannel.key.send('hi');
 });
+
 
 client.on('debug', console.log);
 
@@ -49,12 +52,12 @@ for (const file of commandFiles) {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 	const command = client.commands.get(interaction.commandName);
-    
+
 	if (!command) return;
-    
+
 	try {
 		await command.execute(interaction);
-        console.log('Command executed: ', interaction.commandName );
+		console.log('Command executed: ', interaction.commandName);
 	} catch (error) {
 		console.error(error);
 		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
@@ -62,76 +65,7 @@ client.on('interactionCreate', async interaction => {
 });
 
 // Audit logs
-client.on('messageDelete', async message => {
-	// Ignore direct messages
-	if (!message.guild) return;
-	const fetchedLogs = await message.guild.fetchAuditLogs({
-		limit: 1,
-		type: 'MESSAGE_DELETE',
-	});
-	// Since there's only 1 audit log entry in this collection, grab the first one
-	const deletionLog = fetchedLogs.entries.first();
-	
-	// Perform a coherence check to make sure that there's *something*
-	if (!deletionLog) return console.log(`A message by ${message.author.tag} was deleted, but no relevant audit logs were found.`);
-	
-	// Now grab the user object of the person who deleted the message
-	// Also grab the target of this action to double-check things
-	const { executor, target } = deletionLog;
 
-	// Update the output with a bit more information
-	// Also run a check to make sure that the log returned was for the same author's message
-	if (target.id === message.author.id) {
-		console.log(`A message by ${message.author.tag} was deleted by ${executor.tag}.`);
-	} else {
-		console.log(`A message by ${message.author.tag} was deleted, but we don't know by who.`);
-	}
-});
-
-client.on('guildMemberRemove', async member => {
-	const fetchedLogs = await member.guild.fetchAuditLogs({
-		limit: 1,
-		type: 'MEMBER_KICK',
-	});
-	// Since there's only 1 audit log entry in this collection, grab the first one
-	const kickLog = fetchedLogs.entries.first();
-
-	// Perform a coherence check to make sure that there's *something*
-	if (!kickLog) return console.log(`${member.user.tag} left the guild, most likely of their own will.`);
-
-	// Now grab the user object of the person who kicked the member
-	// Also grab the target of this action to double-check things
-	const { executor, target } = kickLog;
-
-	// Update the output with a bit more information
-	// Also run a check to make sure that the log returned was for the same kicked member
-	if (target.id === member.id) {
-		console.log(`${member.user.tag} left the guild; kicked by ${executor.tag}?`);
-	} else {
-		console.log(`${member.user.tag} left the guild, audit log fetch was inconclusive.`);
-	}
-});
-
-client.on('guildBanAdd', async ban => {
-	const fetchedLogs = await ban.guild.fetchAuditLogs({
-		limit: 1,
-		type: 'MEMBER_BAN_ADD',
-	});
-	// Since there's only 1 audit log entry in this collection, grab the first one
-	const banLog = fetchedLogs.entries.first();
-
-	// Perform a coherence check to make sure that there's *something*
-	if (!banLog) return console.log(`${ban.user.tag} was banned from ${ban.guild.name} but no audit log could be found.`);
-
-	// Now grab the user object of the person who banned the member
-	// Also grab the target of this action to double-check things
-	const { executor, target } = banLog;
-
-	// Update the output with a bit more information
-	// Also run a check to make sure that the log returned was for the same banned member
-	if (target.id === ban.user.id) {
-		console.log(`${ban.user.tag} got hit with the swift hammer of justice in the guild ${ban.guild.name}, wielded by the mighty ${executor.tag}`);
-	} else {
-		console.log(`${ban.user.tag} got hit with the swift hammer of justice in the guild ${ban.guild.name}, audit log fetch was inconclusive.`);
-	}
+client.on('messageDelete', message => {
+	console.log(`A message by ${message.author.tag} was deleted, but we don't know by who yet.`);
 });
