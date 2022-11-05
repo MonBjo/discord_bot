b35d483ea803d36f2e90670a095356672aa68e4c
Monica Björk <m.bjork95@gmail.com>
2022-11-05 15:27:23 +0100

    code should be exactly as the guide

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-import { readdirSync } from 'node:fs'; // Reads command files
-import { join } from 'node:path'; // Creates paths between files
-import { Client, Collection, GatewayIntentBits } from 'discord.js';
-
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.commands = new Collection(); 
 
-const commandsPath = 'commands';
-const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+/* COMMAND HANDELING */
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
 for (const file of commandFiles) {
-	const filePath = join(commandsPath, file);
+	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+	client.commands.set(command.data.name, command);
 }
 
+client.once(Events.ClientReady, () => {
+	console.log('Ready!');
+});
 
-const eventsPath = 'events';
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
+/* === EVENT HANDELING === */
+
+const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
 for (const file of eventFiles) {
-	const filePath = join(eventsPath, file);
+	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
@@ -32,4 +52,4 @@ for (const file of eventFiles) {
 	}
 }
 
-client.login(config.token);
+client.login(token);
